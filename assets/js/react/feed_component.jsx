7b281d257733a429642f4c5e6aa65cfc71b570d4
Adam Adams <ajadams@gmail.com>
2016-02-12 17:28:56 -0800

    adding full view panel

--- assets/js/react/feed_component.jsx
+++ assets/js/react/feed_component.jsx
@@ -17,7 +17,7 @@ var Feed_Component = React.createClass({
 	    function initialize() {
 	    	//google feed api
 	      	var feed = new google.feeds.Feed("http://feeds.feedburner.com/tedtalks_video");
-	      	feed.setNumEntries(10);
+	      	feed.setNumEntries(20);
 	      	feed.load(function(result) {
 		        if (!result.error) {
 					for (var i = 0; i < result.feed.entries.length; i++) {
@@ -35,37 +35,66 @@ var Feed_Component = React.createClass({
 	    google.setOnLoadCallback(initialize);
 	},
 
+	fullView: function(elem){
+		elem.css('border', '3px solid red');
+		var data = elem.data('id');
+		console.log(data)
+	},
+
 	render: function(){
-		var timeago = null;
+		var timeago = null, _this = this;
 		return (
-			<div className={'feed-items-container'}>
-				<ul>
-					{
-						this.state.feed_items.length > 0 ?
-						this.state.feed_items.map(function(val, index, arr){
-							console.log(val);
-							timeago = moment(val.publishedDate).fromNow();
-							return <FeedItem title={val.title}
-											 img={val.mediaGroups[0].contents[0].thumbnails[0].url}
-											 published={timeago}
-											 css={'feed-item clearfix'} /> ;
-						}) :
-						<li>nothing</li>
-					}
-				</ul>
+			<div className="container-fluid">
+				<div className="row">
+					<div className="col-sm-6">
+						<div className={'feed-items-container'}>
+							<ul>
+								{
+									this.state.feed_items.length > 0 ?
+									this.state.feed_items.map(function(val, index, arr){
+										timeago = moment(val.publishedDate).fromNow();
+										return <FeedItem title={val.title}
+														 img={val.mediaGroups[0].contents[0].thumbnails[0].url}
+														 published={timeago}
+														 id={index}
+														 fullView={_this.fullView}
+														 key={index}
+														 css={'feed-item clearfix'} /> ;
+									}) :
+									<li></li>
+								}
+							</ul>
+						</div>
+					</div>
+					<div className="col-sm-6 full-view">
+						<h3 className="text-center"><b>TED</b><span>Talk</span> <small>Feed</small></h3>
+						<div className="view">
+							view here
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
 });
 
 var FeedItem = React.createClass({
+	makeActive: function(e){
+		e.preventDefault();
+		var elem = $(e.currentTarget).closest('.feed-item');
+		console.log(elem);
+		this.props.fullView(elem);
+	},
+
 	render: function(){
 		return(
-			<li className={this.props.css}>
+			<li className={this.props.css} data-id={this.props.id}>
 				<img className="thumb" src={this.props.img} />
 				<div className="content">
-					<div className="title">{this.props.title}</div>
-					<div className="timeago"><small>Posted: {this.props.published}</small></div>
+					<a href="" onClick={this.makeActive}>
+						<div className="title">{this.props.title}</div>
+						<div className="timeago"><small>Posted: {this.props.published}</small></div>
+					</a>
 				</div>
 			</li>
 		);
--- public/js/feed_component.js
+++ public/js/feed_component.js
@@ -17,7 +17,7 @@ var Feed_Component = React.createClass({displayName: "Feed_Component",
 	    function initialize() {
 	    	//google feed api
 	      	var feed = new google.feeds.Feed("http://feeds.feedburner.com/tedtalks_video");
-	      	feed.setNumEntries(10);
+	      	feed.setNumEntries(20);
 	      	feed.load(function(result) {
 		        if (!result.error) {
 					for (var i = 0; i < result.feed.entries.length; i++) {
@@ -35,23 +35,43 @@ var Feed_Component = React.createClass({displayName: "Feed_Component",
 	    google.setOnLoadCallback(initialize);
 	},
 
+	fullView: function(elem){
+		elem.css('border', '3px solid red');
+		var data = elem.data('id');
+		console.log(data)
+	},
+
 	render: function(){
-		var timeago = null;
+		var timeago = null, _this = this;
 		return (
-			React.createElement("div", {className: 'feed-items-container'}, 
-				React.createElement("ul", null, 
-					
-						this.state.feed_items.length > 0 ?
-						this.state.feed_items.map(function(val, index, arr){
-							console.log(val);
-							timeago = moment(val.publishedDate).fromNow();
-							return React.createElement(FeedItem, {title: val.title, 
-											 img: val.mediaGroups[0].contents[0].thumbnails[0].url, 
-											 published: timeago, 
-											 css: 'feed-item clearfix'}) ;
-						}) :
-						React.createElement("li", null, "nothing")
-					
+			React.createElement("div", {className: "container-fluid"}, 
+				React.createElement("div", {className: "row"}, 
+					React.createElement("div", {className: "col-sm-6"}, 
+						React.createElement("div", {className: 'feed-items-container'}, 
+							React.createElement("ul", null, 
+								
+									this.state.feed_items.length > 0 ?
+									this.state.feed_items.map(function(val, index, arr){
+										timeago = moment(val.publishedDate).fromNow();
+										return React.createElement(FeedItem, {title: val.title, 
+														 img: val.mediaGroups[0].contents[0].thumbnails[0].url, 
+														 published: timeago, 
+														 id: index, 
+														 fullView: _this.fullView, 
+														 key: index, 
+														 css: 'feed-item clearfix'}) ;
+									}) :
+									React.createElement("li", null)
+								
+							)
+						)
+					), 
+					React.createElement("div", {className: "col-sm-6 full-view"}, 
+						React.createElement("h3", {className: "text-center"}, React.createElement("b", null, "TED"), React.createElement("span", null, "Talk"), " ", React.createElement("small", null, "Feed")), 
+						React.createElement("div", {className: "view"}, 
+							"view here"
+						)
+					)
 				)
 			)
 		);
@@ -59,13 +79,22 @@ var Feed_Component = React.createClass({displayName: "Feed_Component",
 });
 
 var FeedItem = React.createClass({displayName: "FeedItem",
+	makeActive: function(e){
+		e.preventDefault();
+		var elem = $(e.currentTarget).closest('.feed-item');
+		console.log(elem);
+		this.props.fullView(elem);
+	},
+
 	render: function(){
 		return(
-			React.createElement("li", {className: this.props.css}, 
+			React.createElement("li", {className: this.props.css, "data-id": this.props.id}, 
 				React.createElement("img", {className: "thumb", src: this.props.img}), 
 				React.createElement("div", {className: "content"}, 
-					React.createElement("div", {className: "title"}, this.props.title), 
-					React.createElement("div", {className: "timeago"}, React.createElement("small", null, "Posted: ", this.props.published))
+					React.createElement("a", {href: "", onClick: this.makeActive}, 
+						React.createElement("div", {className: "title"}, this.props.title), 
+						React.createElement("div", {className: "timeago"}, React.createElement("small", null, "Posted: ", this.props.published))
+					)
 				)
 			)
 		);
